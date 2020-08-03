ae435e370f4cf1a841186fc51f5b785570931acd
Caglar Gonul <caglar.gonul.001@gmail.com>
2020-08-03 18:35:02 +0300

    Waiting asynchronously from a CompletionStage.

--- src/main/java/com/akka/demo/a009/Main.java
+++ src/main/java/com/akka/demo/a009/Main.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 import java.time.Duration;
 import java.util.SortedSet;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
 
 import akka.actor.typed.ActorSystem;
 import akka.actor.typed.javadsl.AskPattern;
@@ -12,16 +13,20 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
 		ActorSystem<ManagerBehavior.Command> bigPrimes = ActorSystem.create(ManagerBehavior.create(), "BigPrimes");
-		CompletionStage<SortedSet<BigInteger>> result = AskPattern.ask(bigPrimes, (me) -> new ManagerBehavior.InstructionCommand("start", me), Duration.ofSeconds(100), bigPrimes.scheduler());
+		CompletionStage<SortedSet<BigInteger>> result = AskPattern.ask(bigPrimes, (me) -> new ManagerBehavior.InstructionCommand("start", me), Duration.ofSeconds(10), bigPrimes.scheduler());
 		result.whenComplete((reply, failure) -> {
 			if(reply != null){
 				reply.forEach(s -> log.info(s.toString()));
 			} else {
 				log.error("The system does not respond in time");
+				bigPrimes.terminate();
+				throw new RuntimeException("The system does not respond in time");
 			}
 			bigPrimes.terminate();
 		});
+		SortedSet<BigInteger> bigIntegers = result.toCompletableFuture().get();
+		log.info(bigIntegers.toString());
 	}
 }
